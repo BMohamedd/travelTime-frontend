1054ce731ec666a9d9614c191f79d53caab4a3c5
Belhadj Mohamed <rafikgamer8@gmail.com>
2022-05-04 14:28:28 +0200

    just changed the url form localhost

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "https://trave-time-back-end.herokuapp.com/";
 
 export const login = (userdata) => axios.post(`${url}/login`, userdata);
 export const register = (userdata) => axios.post(`${url}/register`, userdata);
